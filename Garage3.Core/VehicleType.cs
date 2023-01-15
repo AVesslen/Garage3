575bb1b8507c2e58e3542fe7181e64a6c57e6ca8
soroshdotnet <sorosh.farahmand@gmail.com>
2023-01-15 19:08:08 +0100

    Layout och db

--- Garage3.Core/VehicleType.cs
+++ Garage3.Core/VehicleType.cs
@@ -4,12 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+#nullable disable
+
 namespace Garage3.Core
 {
     public class VehicleType
     {
         public int Id { get; set; }
         public string Type { get; set; } = string.Empty;
-        public ICollection<Vehicle> Vehicles { get; set; } = new List<Vehicle>();
+        public ICollection<Vehicle> Vehicles { get; set; } // = new List<Vehicle>();
     }
 }
